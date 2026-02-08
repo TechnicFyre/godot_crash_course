db18ebf93a0ea3cb1d2dddfe625aac4a0b825428
Wil <36526831+TechnicFyre@users.noreply.github.com>
2026-02-07 22:05:58 -0600

    State after video 2, coding in Godot.

---
+++ scenes/player.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var speed: int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100,100)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("left","right","up","down")
+	position += direction * speed * delta
