94bf54401522b1895272cf1f068bd1da0ab4ad64
Wil <36526831+TechnicFyre@users.noreply.github.com>
2026-02-17 19:03:12 -0600

    State after video 3, physics & signals.

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,15 +1,15 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var speed: int = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = Vector2(100,100)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = Input.get_vector("left","right","up","down")
-	velocity = direction * speed
-	move_and_slide()
-	
+	pass
+
+
+func _on_timer_timeout() -> void:
+	print('meteor')
