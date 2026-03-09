5becfa8458d13a2d2486e8a606ebf3961713d202
Wil <36526831+TechnicFyre@users.noreply.github.com>
2026-03-09 16:04:18 -0500

    State after video 6, animation.

--- scenes/laser.gd
+++ scenes/laser.gd
@@ -5,5 +5,9 @@ func _on_area_entered(_area: Area2D) -> void:
 
 @export var speed = 500
 
+func _ready():
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, 'scale', Vector2(1,1), 0.2).from(Vector2(0,0.2))
+
 func _process(delta: float) -> void:
 	position.y -= speed * delta
--- scenes/level.gd
+++ scenes/level.gd
@@ -4,6 +4,38 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://scenes/laser.tscn")
 
+var rng := RandomNumberGenerator.new()
+
+func _ready() -> void:
+	# for calculating positions later
+	var width := get_viewport_rect().size[0]
+	var height := get_viewport_rect().size[1]
+	var random_x : int
+	var random_y : int
+	
+	# Spawn in random stars
+	var star : AnimatedSprite2D
+	var count := rng.randi_range(20,30)
+	for i in range(count):
+		# copy the invisible star, make it visible
+		star = $Stars/Star.duplicate()
+		star.visible = true
+		
+		# randomize position
+		random_x = rng.randi_range(0, int(width))
+		random_y = rng.randi_range(0, int(height))
+		star.position = Vector2(random_x, random_y)
+		
+		# randomize size, default is 1.0
+		var size = rng.randf_range(0.3, 0.8)
+		star.scale = Vector2(size, size)
+		
+		# randomize animation speed, default is 1.0
+		star.speed_scale = rng.randf_range(0.5,2)
+		
+		# add it to Stars
+		$Stars.add_child(star)
+
 func _on_meteor_timer_timeout() -> void:
 	# 2. create an instance
 	var meteor = meteor_scene.instantiate()
